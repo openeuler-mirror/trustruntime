9f5b9b07eb6a91a29263dcde3136855269763e26
SunShuoJia <846134098@qq.com>
2026-09-24 11:19:22 +0800

    fix:修复capable、filesystem路径匹配通配符失效问题

--- rust/plugins/agentsandbox/security/src/bpf/capability.bpf.c
+++ rust/plugins/agentsandbox/security/src/bpf/capability.bpf.c
@@ -24,7 +24,7 @@ static __always_inline int path_prefix_match(const char *path, const char *prefi
         __u64 z = ~b & (b - 0x0101010101010101ULL) & 0x8080808080808080ULL;
         if (z) {
             __u64 lowest = z & (~z + 1);
-            __u64 mask = lowest | (lowest - 1);
+            __u64 mask = (lowest >> 7) - 1;
             if ((a & mask) != (b & mask)) {
                 return 0;
             }
